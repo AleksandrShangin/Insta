23245f1760ea093a74d2e4899e9af96d06c0e432
Alex <ashangin@hotmail.com>
2021-03-19 21:42:12 +0300

    First commit

---
+++ Insta/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Alex on 3/19/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
